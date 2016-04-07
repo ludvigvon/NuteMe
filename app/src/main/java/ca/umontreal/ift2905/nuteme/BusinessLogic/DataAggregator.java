ff8934ace13f911fb00581397d71badea38e4f42
Hani Dagher <ludvigvon@gmail.com>
2016-04-07 11:33:26 -0400

    commentaires ajoutés avec Leroy

--- app/src/main/java/ca/umontreal/ift2905/nuteme/BusinessLogic/DataAggregator.java
+++ app/src/main/java/ca/umontreal/ift2905/nuteme/BusinessLogic/DataAggregator.java
@@ -1,7 +1,30 @@
 package ca.umontreal.ift2905.nuteme.BusinessLogic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.umontreal.ift2905.nuteme.DataModel.Ingredient;
+import ca.umontreal.ift2905.nuteme.DataModel.Nutrient;
+import ca.umontreal.ift2905.nuteme.DataModel.Recipe;
+
 /**
  * Created by h on 25/03/16.
  */
 public class DataAggregator {
+    /* TODO: ajouter une méthode qui reçoit une liste de recettes fait une agrégation sur les
+           ingrédients et retourne une liste d'ingrédients. Pour chaque ingrédient, on veut avoir
+           le ratio de chacune des recettes et les principaux éléments nutritionnels
+    */
+    public List<Ingredient> getAggregateViewByIngredient(List<Recipe> recipes){
+
+        return new ArrayList<Ingredient>();
+    }
+
+    /* TODO: ajouter une méthode qui reçoit une liste de recettes fait une agrégation sur les
+           nutriments et retourne une liste de nutriments. Pour chaque nutriment, on veut avoir
+           les principaux ingrédients qui participents à leur valeur
+    */
+    public List<Nutrient> getAggregateViewByNutrient(List<Recipe> recipes){
+        return new ArrayList<Nutrient>();
+    }
 }
