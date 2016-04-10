2bf11c5af93f51878218b71a62f942c632778f5a
Hani Dagher <ludvigvon@gmail.com>
2016-04-10 10:45:07 -0400

    NewSearch button in DetailedViews

--- app/src/main/java/ca/umontreal/ift2905/nuteme/DetailedViews.java
+++ app/src/main/java/ca/umontreal/ift2905/nuteme/DetailedViews.java
@@ -1,5 +1,6 @@
 package ca.umontreal.ift2905.nuteme;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -9,17 +10,22 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
 import ca.umontreal.ift2905.nuteme.DataAccess.APIHelper;
 import ca.umontreal.ift2905.nuteme.DataModel.Recipe;
 
-public class DetailedViews extends AppCompatActivity  {
+public class DetailedViews extends AppCompatActivity implements View.OnClickListener {
 
     TabLayout tablayout;
     ViewPager pager;
     List<Recipe> recipesData;
+    Button newSearchButton;
+
 
     public static final String[] TAB_TITLES = new String[] {"Recipes", "Ingredients", "Nutrients"};
     public static final String DETAILED_VIEWS_POS = "DetailedViewsPosition";
@@ -33,6 +39,9 @@ public class DetailedViews extends AppCompatActivity  {
 
         tablayout = (TabLayout)findViewById(R.id.detailedViews_tablayout);
         pager = (ViewPager)findViewById(R.id.detailedViews_pager);
+        newSearchButton = (Button)findViewById(R.id.detailedViews_button_newSearch);
+
+        newSearchButton.setOnClickListener(this);
 
         recipesData = APIHelper.getInstance().getDetailedRecipes();
 
@@ -45,6 +54,12 @@ public class DetailedViews extends AppCompatActivity  {
 
     public List<Recipe> getRecipes(){
         return recipesData;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MenuMain.class);
+        startActivity(intent);
     }
 
     public class DetailedViewsPagerAdapter extends FragmentPagerAdapter {
