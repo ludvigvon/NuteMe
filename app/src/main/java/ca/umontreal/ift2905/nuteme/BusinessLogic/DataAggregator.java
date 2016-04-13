b3f713a153b058c4aee482fc4a6bdeb154034b4c
Hani Dagher <ludvigvon@gmail.com>
2016-04-12 23:09:05 -0400

    Avancement - agrégations par ingrédient et par nutriments et tests unitaires

--- app/src/main/java/ca/umontreal/ift2905/nuteme/BusinessLogic/DataAggregator.java
+++ app/src/main/java/ca/umontreal/ift2905/nuteme/BusinessLogic/DataAggregator.java
@@ -11,6 +11,8 @@ import java.util.Map;
 
 import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.GenericAggregation;
 import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.IngredientNutrients;
+import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.IngredientRecipes;
+import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.NutrientIngredients;
 import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.NutrientRecipes;
 import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.RecipeRatio;
 import ca.umontreal.ift2905.nuteme.DataModel.Ingredient;
@@ -25,23 +27,23 @@ public class DataAggregator {
 
     /**
      * Aggregates nutritional values for a list of recipes
+     *
      * @param recipes A list of recipes with nutritional data
      * @return List<Nutrient> Aggregated list of nutrients ordered by nutrient name
      */
-    public List<Nutrient> getSummaryAggregateView(List<Recipe> recipes){
+    public List<Nutrient> getSummaryAggregateView(List<Recipe> recipes) {
         Map<String, Nutrient> dict = new HashMap<>();
 
-        for (Recipe recipe : recipes){
-            for (Nutrient nutrient : recipe.nutrition.nutrients){
+        for (Recipe recipe : recipes) {
+            for (Nutrient nutrient : recipe.nutrition.nutrients) {
                 String key = nutrient.title;
-                if (dict.containsKey(key)){
+                if (dict.containsKey(key)) {
                     Nutrient value = dict.get(key);
                     if (value.unit == nutrient.unit)
                         value.amount += nutrient.amount;
                     else
                         Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
-                }
-                else{
+                } else {
                     dict.put(key, nutrient);
                 }
             }
@@ -56,91 +58,41 @@ public class DataAggregator {
             }
         });
 
-        return  result;
+        return result;
     }
+
 
     /**
      *
      * @param recipes
      * @return
      */
-    public GenericAggregation<IngredientNutrients> getAggregateViewByIngredient(List<Recipe> recipes){
+    public GenericAggregation<IngredientNutrients> getAggregateViewByIngredient(List<Recipe> recipes) {
+        // TODO: (Hani) add if > 0
+        Map<String, List<IngredientNutrients>> dictIngredientNutrients = extractIngredients(recipes);
+
+        GenericAggregation<IngredientNutrients> result = new GenericAggregation<>(aggregateIngredientNutrients(dictIngredientNutrients));
+
+        return result;
+    }
+
+    private Map<String, List<IngredientNutrients>> extractIngredients(List<Recipe> recipes) {
         Map<String, List<IngredientNutrients>> dict = new HashMap<>();
 
-        // TODO: (Hani) add if > 0
-
-        for (Recipe recipe : recipes){
-            for (Ingredient ingredient : recipe.nutrition.ingredients){
+        for (Recipe recipe : recipes) {
+            for (Ingredient ingredient : recipe.nutrition.ingredients) {
                 String key = ingredient.name;
-                if (dict.containsKey(key)){
+                if (dict.containsKey(key)) {
                     List<IngredientNutrients> value = dict.get(key);
                     mapIngredientNutrients(recipe, ingredient, value);
-                }
-                else{
+                } else {
                     List<IngredientNutrients> list = new ArrayList<IngredientNutrients>();
                     mapIngredientNutrients(recipe, ingredient, list);
                     dict.put(key, list);
                 }
             }
         }
-
-        // fusionner les listes pour un même ingrédient
-        for (Map.Entry<String, List<IngredientNutrients>> entry : dict.entrySet() ){
-            Map<String, NutrientRecipes> nutrientRecipesDict = new HashMap<>();
-
-            List<IngredientNutrients> value = entry.getValue();
-            IngredientNutrients first = value.get(0);
-
-            for (int i=1; i < value.size()   ; i++){
-                IngredientNutrients next = value.get(i);
-                if (next.unit == first.unit) {
-                    first.amount += next.amount;
-                    first.aggregatedList.addAll(next.aggregatedList);
-
-                    for (NutrientRecipes nutrientRecipes : first.aggregatedList){
-                        String key = nutrientRecipes.name;
-                        if (nutrientRecipesDict.containsKey(key)){
-                            NutrientRecipes nutrientRecipesValue = nutrientRecipesDict.get(key);
-                            if (nutrientRecipesValue.unit == nutrientRecipes.unit) {
-                                nutrientRecipesValue.amount += nutrientRecipes.amount;
-                                nutrientRecipesValue.aggregatedList.addAll(nutrientRecipes.aggregatedList);
-                            }
-                            else
-                                Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
-                        }
-                        else{
-                            nutrientRecipesDict.put(key,nutrientRecipes);
-                        }
-                    }
-                }
-                else
-                    Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
-            }
-            first.aggregatedList = new ArrayList<>(nutrientRecipesDict.values());
-        }
-
-        // clean up
-
-
-        List<List<IngredientNutrients>> result = new ArrayList<>(dict.values());
-
-
-        for (List<IngredientNutrients> list : result){
-
-        }
-
-
-        /* from this list of lists of IngredientNutrients compute a single list of  IngredientNutrients encapsulated in GenericAggregation<IngredientNutrients> */
-
-
-//        Collections.sort(result, new Comparator<Nutrient>() {
-//            @Override
-//            public int compare(Nutrient lhs, Nutrient rhs) {
-//                return lhs.title.compareTo(rhs.title);
-//            }
-//        });
-
-        return  new GenericAggregation<>(result.get(0));
+        return dict;
     }
 
     private void mapIngredientNutrients(Recipe recipe, Ingredient ingredient, List<IngredientNutrients> list) {
@@ -164,12 +116,233 @@ public class DataAggregator {
         }
     }
 
+    private List<IngredientNutrients> aggregateIngredientNutrients(Map<String, List<IngredientNutrients>> dictIngredientNutrients) {
+        List<IngredientNutrients> result = new ArrayList<>();
+
+        for (Map.Entry<String, List<IngredientNutrients>> entry : dictIngredientNutrients.entrySet()) {
+            Map<String, List<NutrientRecipes>> dictNutrientRecipes = new HashMap<>();
+
+            List<IngredientNutrients> value = entry.getValue();
+            IngredientNutrients first = value.get(0);
+
+            result.add(first);
+
+            for (int i = 1; i < value.size(); i++) {
+                IngredientNutrients next = value.get(i);
+                if (next.unit == first.unit) {
+                    first.amount += next.amount;
+                    first.aggregatedList.addAll(next.aggregatedList);
+                } else
+                    Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
+            }
+            dictNutrientRecipes.clear();
+            for (NutrientRecipes nutrientRecipes : first.aggregatedList) {
+                String key = nutrientRecipes.name;
+                if (dictNutrientRecipes.containsKey(key)) {
+                    List<NutrientRecipes> nutrientRecipesList = dictNutrientRecipes.get(key);
+                    nutrientRecipesList.add(nutrientRecipes);
+                } else {
+                    List<NutrientRecipes> listNutrientRecipes = new ArrayList<NutrientRecipes>();
+                    listNutrientRecipes.add(nutrientRecipes);
+                    dictNutrientRecipes.put(key, listNutrientRecipes);
+                }
+            }
+            first.aggregatedList = aggregateNutrientRecipes(dictNutrientRecipes);
+        }
+
+        return result;
+    }
+
+    private List<NutrientRecipes> aggregateNutrientRecipes(Map<String, List<NutrientRecipes>> dict) {
+        List<NutrientRecipes> result = new ArrayList<>();
+
+        for (Map.Entry<String, List<NutrientRecipes>> entry : dict.entrySet()) {
+            Map<String, List<RecipeRatio>> dictRecipeRatio = new HashMap<>();
+
+            List<NutrientRecipes> value = entry.getValue();
+            NutrientRecipes first = value.get(0);
+
+            result.add(first);
+
+            for (int i = 1; i < value.size(); i++) {
+                NutrientRecipes next = value.get(i);
+                if (next.unit == first.unit) {
+                    first.amount += next.amount;
+                    first.aggregatedList.addAll(next.aggregatedList);
+                } else
+                    Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
+            }
+            dictRecipeRatio.clear();
+            for (RecipeRatio recipeRatio : first.aggregatedList) {
+                String key = recipeRatio.name;
+                if (dictRecipeRatio.containsKey(key)) {
+                    List<RecipeRatio> recipeRatioList = dictRecipeRatio.get(key);
+                    recipeRatioList.add(recipeRatio);
+                } else {
+                    List<RecipeRatio> recipeRatioList = new ArrayList<RecipeRatio>();
+                    recipeRatioList.add(recipeRatio);
+                    dictRecipeRatio.put(key, recipeRatioList);
+                }
+            }
+
+            first.aggregatedList = aggregateRecipeRatios(dictRecipeRatio);
+
+        }
+
+        return result;
+    }
+
+    private List<RecipeRatio> aggregateRecipeRatios(Map<String, List<RecipeRatio>> dictRecipeRatio) {
+        List<RecipeRatio> result = new ArrayList<>();
+
+        for (Map.Entry<String, List<RecipeRatio>> entry : dictRecipeRatio.entrySet()) {
+            Map<String, List<RecipeRatio>> dictRatios = new HashMap<>();
+
+            List<RecipeRatio> value = entry.getValue();
+            RecipeRatio first = value.get(0);
+
+            result.add(first);
+
+            for (int i = 1; i < value.size(); i++) {
+                RecipeRatio next = value.get(i);
+            }
+        }
+        return result;
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      *
      * @param recipes
      * @return
      */
-    public List<Nutrient> getAggregateViewByNutrient(List<Recipe> recipes){
-        return new ArrayList<Nutrient>();
+    public GenericAggregation<NutrientIngredients>  getAggregateViewByNutrient(List<Recipe> recipes){
+        // TODO: (Hani) add if > 0
+        Map<String, List<NutrientIngredients>> dictNutrientIngredients = extractNutrients(recipes);
+
+        GenericAggregation<NutrientIngredients> result = new GenericAggregation<>(aggregateNutrientIngredients(dictNutrientIngredients));
+
+        return result;
     }
+
+    private Map<String, List<NutrientIngredients>> extractNutrients(List<Recipe> recipes) {
+        Map<String, List<NutrientIngredients>> dict = new HashMap<>();
+
+        for (Recipe recipe : recipes) {
+            for (Ingredient ingredient : recipe.nutrition.ingredients) {
+                for (SimpleNutrient nutrient : ingredient.nutrients) {
+                    String key = nutrient.name;
+                    if (dict.containsKey(key)) {
+                        List<NutrientIngredients> value = dict.get(key);
+                        mapNutrientIngredients(recipe, ingredient, nutrient, value);
+                    } else {
+                        List<NutrientIngredients> list = new ArrayList<>();
+                        mapNutrientIngredients(recipe, ingredient, nutrient, list);
+                        dict.put(key, list);
+                    }
+                }
+            }
+        }
+        return dict;
+    }
+
+    private void mapNutrientIngredients(Recipe recipe, Ingredient ingredient, SimpleNutrient nutrient, List<NutrientIngredients> list) {
+        NutrientIngredients item = new NutrientIngredients();
+        list.add(item);
+        item.name = nutrient.name;
+        item.amount = nutrient.amount;
+        item.aggregatedList = new ArrayList<IngredientRecipes>();
+
+        IngredientRecipes ir = new IngredientRecipes();
+        ir.name = ingredient.name;
+        ir.amount = ingredient.amount;
+        ir.unit = ingredient.unit;
+        ir.aggregatedList = new ArrayList<RecipeRatio>();
+
+        RecipeRatio ratio = new RecipeRatio();
+        ratio.name = recipe.title;
+
+        item.aggregatedList.add(ir);
+        ir.aggregatedList.add(ratio);
+    }
+
+    private List<NutrientIngredients> aggregateNutrientIngredients(Map<String, List<NutrientIngredients>> dictNutrientIngredients) {
+        List<NutrientIngredients> result = new ArrayList<>();
+
+        for (Map.Entry<String, List<NutrientIngredients>> entry : dictNutrientIngredients.entrySet()) {
+            Map<String, List<IngredientRecipes>> dictIngredientRecipes = new HashMap<>();
+
+            List<NutrientIngredients> value = entry.getValue();
+            NutrientIngredients first = value.get(0);
+
+            result.add(first);
+
+            for (int i = 1; i < value.size(); i++) {
+                NutrientIngredients next = value.get(i);
+                if (next.unit == first.unit) {
+                    first.amount += next.amount;
+                    first.aggregatedList.addAll(next.aggregatedList);
+                } else
+                    Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
+            }
+            dictIngredientRecipes.clear();
+            for (IngredientRecipes ingredientRecipes : first.aggregatedList) {
+                String key = ingredientRecipes.name;
+                if (dictIngredientRecipes.containsKey(key)) {
+                    List<IngredientRecipes> ingredientRecipesList = dictIngredientRecipes.get(key);
+                    ingredientRecipesList.add(ingredientRecipes);
+                } else {
+                    List<IngredientRecipes> listIngredientRecipes = new ArrayList<>();
+                    listIngredientRecipes.add(ingredientRecipes);
+                    dictIngredientRecipes.put(key, listIngredientRecipes);
+                }
+            }
+            first.aggregatedList = aggregateIngredientRecipes(dictIngredientRecipes);
+        }
+
+        return result;
+    }
+
+
+    private List<IngredientRecipes> aggregateIngredientRecipes(Map<String, List<IngredientRecipes>> dict) {
+        List<IngredientRecipes> result = new ArrayList<>();
+
+        for (Map.Entry<String, List<IngredientRecipes>> entry : dict.entrySet()) {
+            Map<String, List<RecipeRatio>> dictRecipeRatio = new HashMap<>();
+
+            List<IngredientRecipes> value = entry.getValue();
+            IngredientRecipes first = value.get(0);
+
+            result.add(first);
+
+            for (int i = 1; i < value.size(); i++) {
+                IngredientRecipes next = value.get(i);
+                if (next.unit == first.unit) {
+                    first.amount += next.amount;
+                    first.aggregatedList.addAll(next.aggregatedList);
+                } else
+                    Log.d("DataAggregator", "Unit mismatch in getSummaryAggregateView");
+            }
+            dictRecipeRatio.clear();
+            for (RecipeRatio recipeRatio : first.aggregatedList) {
+                String key = recipeRatio.name;
+                if (dictRecipeRatio.containsKey(key)) {
+                    List<RecipeRatio> recipeRatioList = dictRecipeRatio.get(key);
+                    recipeRatioList.add(recipeRatio);
+                } else {
+                    List<RecipeRatio> recipeRatioList = new ArrayList<RecipeRatio>();
+                    recipeRatioList.add(recipeRatio);
+                    dictRecipeRatio.put(key, recipeRatioList);
+                }
+            }
+
+            first.aggregatedList = aggregateRecipeRatios(dictRecipeRatio);
+
+        }
+
+        return result;
+    }
+
 }
--- app/src/test/java/ca/umontreal/ift2905/nuteme/DataAggregatorTest.java
+++ app/src/test/java/ca/umontreal/ift2905/nuteme/DataAggregatorTest.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 import ca.umontreal.ift2905.nuteme.BusinessLogic.DataAggregator;
 import ca.umontreal.ift2905.nuteme.DataAccess.APIHelper;
+import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.GenericAggregation;
+import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.IngredientNutrients;
+import ca.umontreal.ift2905.nuteme.DataModel.Aggregations.NutrientIngredients;
 import ca.umontreal.ift2905.nuteme.DataModel.Ingredient;
 import ca.umontreal.ift2905.nuteme.DataModel.Nutrient;
 import ca.umontreal.ift2905.nuteme.DataModel.Nutrition;
@@ -50,22 +53,22 @@ public class DataAggregatorTest {
         return nutrients;
     }
 
-    public List<SimpleNutrient> getMockListSimpleNutrients(){
+    public List<SimpleNutrient> getMockListSimpleNutrients(int[] vals){
         List<SimpleNutrient> nutrients = new ArrayList<>();
         SimpleNutrient nutrient1 = new SimpleNutrient();
         SimpleNutrient nutrient2 = new SimpleNutrient();
         SimpleNutrient nutrient3 = new SimpleNutrient();
 
         nutrient1.name = "nutrient1";
-        nutrient1.amount = 10;
+        nutrient1.amount = vals[0];
         nutrient1.unit = "mg";
 
         nutrient2.name = "nutrient2";
-        nutrient2.amount = 20;
+        nutrient2.amount = vals[1];
         nutrient2.unit = "mg";
 
         nutrient3.name = "nutrient3";
-        nutrient3.amount = 30;
+        nutrient3.amount = vals[2];
         nutrient3.unit = "mg";
 
         nutrients.add(nutrient1);
@@ -84,17 +87,17 @@ public class DataAggregatorTest {
         ingredient1.name = "ingredient1";
         ingredient1.amount = 10;
         ingredient1.unit = "mg";
-        ingredient1.nutrients = getMockListSimpleNutrients();
+        ingredient1.nutrients = getMockListSimpleNutrients(new int[]{2,3,5});
 
         ingredient2.name = "ingredient2";
         ingredient2.amount = 20;
         ingredient2.unit = "mg";
-        ingredient2.nutrients = getMockListSimpleNutrients();
+        ingredient2.nutrients = getMockListSimpleNutrients(new int[]{3,5,12});
 
         ingredient3.name = "ingredient3";
         ingredient3.amount = 30;
         ingredient3.unit = "mg";
-        ingredient3.nutrients = getMockListSimpleNutrients();
+        ingredient3.nutrients = getMockListSimpleNutrients(new int[]{8,10,12});
 
         ingredients.add(ingredient1);
         ingredients.add(ingredient2);
@@ -138,19 +141,24 @@ public class DataAggregatorTest {
     @Test
     public void getAggregateViewByIngredient_Success() throws Exception {
 
+
         List<Recipe> recipes = new ArrayList<>();
 
         Recipe recipe1 = new Recipe();
         Recipe recipe2 = new Recipe();
         Recipe recipe3 = new Recipe();
 
+        recipe1.title = "recipe1";
+        recipe2.title = "recipe2";
+        recipe3.title = "recipe3";
+
         recipe1.nutrition = new Nutrition();
         recipe2.nutrition = new Nutrition();
         recipe3.nutrition = new Nutrition();
 
-        recipe1.nutrition.nutrients = getMockListNutrients();
-        recipe2.nutrition.nutrients = getMockListNutrients();
-        recipe3.nutrition.nutrients = getMockListNutrients();
+        recipe1.nutrition.ingredients = getMockListIngredients();
+        recipe2.nutrition.ingredients = getMockListIngredients();
+        recipe3.nutrition.ingredients = getMockListIngredients();
 
         recipes.add(recipe1);
         recipes.add(recipe2);
@@ -158,12 +166,45 @@ public class DataAggregatorTest {
 
         DataAggregator aggregator = new DataAggregator();
 
-        List<Nutrient> result = aggregator.getSummaryAggregateView(recipes);
+        GenericAggregation<IngredientNutrients> result = aggregator.getAggregateViewByIngredient(recipes);
 
 
-        Assert.assertTrue(result.get(0).amount == 30);
-        Assert.assertTrue(result.get(1).amount == 60);
-        Assert.assertTrue(result.get(2).amount == 90);
+        Assert.assertNotNull(result);
+
+    }
+
+    @Test
+    public void getAggregateViewByNutrient_Success() throws Exception {
+
+
+        List<Recipe> recipes = new ArrayList<>();
+
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        Recipe recipe3 = new Recipe();
+
+        recipe1.title = "recipe1";
+        recipe2.title = "recipe2";
+        recipe3.title = "recipe3";
+
+        recipe1.nutrition = new Nutrition();
+        recipe2.nutrition = new Nutrition();
+        recipe3.nutrition = new Nutrition();
+
+        recipe1.nutrition.ingredients = getMockListIngredients();
+        recipe2.nutrition.ingredients = getMockListIngredients();
+        recipe3.nutrition.ingredients = getMockListIngredients();
+
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+        recipes.add(recipe3);
+
+        DataAggregator aggregator = new DataAggregator();
+
+        GenericAggregation<NutrientIngredients> result = aggregator.getAggregateViewByNutrient(recipes);
+
+
+        Assert.assertNotNull(result);
 
     }
 
